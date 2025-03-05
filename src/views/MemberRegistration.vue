ab89d1c5542b3bec5d51b956987a74150e83ef60
amaritraj <webdesign620605@gmail.com>
2025-03-05 10:49:49 +0600

    Registation View Page Update

--- src/views/MemberRegistration.vue
+++ src/views/MemberRegistration.vue
@@ -1,21 +1,28 @@
 <template>
-    <h3>Member Registration Page!</h3>
-    <form>
-        <div class="mb-3">
-            <label for="exampleInputEmail1" class="form-label">Email address</label>
-            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-            <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+    <div class="row justify-content-center">
+        <div class="col-md-6">
+            <h3>Member Registration Page</h3>
+            <form>
+                <div class="mb-3">
+                    <label for="InputName" class="form-label">User Name </label>
+                    <input type="name" name="name" class="form-control" id="InputName">
+                </div>
+                <div class="mb-3">
+                    <label for="InputEmail" class="form-label">User Email </label>
+                    <input type="email" name="email" class="form-control" id="InputEmail">
+                </div>
+                <div class="mb-3">
+                    <label for="InputPassword" class="form-label">User Password</label>
+                    <input type="password" class="form-control" id="InputPassword">
+                </div>
+                <div class="mb-3">
+                    <label for="InputComPassword" class="form-label">Confirmed Password</label>
+                    <input type="password" name="conpassword" class="form-control" id="InputComPassword">
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
         </div>
-        <div class="mb-3">
-            <label for="exampleInputPassword1" class="form-label">Password</label>
-            <input type="password" class="form-control" id="exampleInputPassword1">
-        </div>
-        <div class="mb-3 form-check">
-            <input type="checkbox" class="form-check-input" id="exampleCheck1">
-            <label class="form-check-label" for="exampleCheck1">Check me out</label>
-        </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
-    </form>
+    </div>
 </template>
 <script setup></script>
 <style scoped></style>
